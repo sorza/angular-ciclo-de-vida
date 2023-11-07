9351b98bdaf3359c9bb653ca7b401e60b547a1bb
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2023-11-07 13:08:15 -0300

    Implementando a edição do item

--- life-cycle/src/app/components/input/input.component.ts
+++ life-cycle/src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,7 +7,11 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+
+  @Input() ItemQueSeraEditado!: Item;
+  editando = false;
+  textoBtn = 'Salvar item'
 
   valorItem! : string;
   constructor(private listaService: ListaDeCompraService) { }
@@ -22,5 +27,22 @@ export class InputComponent implements OnInit {
   limparCampo()
   {
     this.valorItem = '';
+  }
+
+  editarItem()
+  {
+    this.listaService.editarItemDaLista(this.ItemQueSeraEditado, this.valorItem);
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = "Salvar item"
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(!changes['ItemQueSeraEditado'].firstChange)
+    {
+      this.editando = true;
+      this.textoBtn = 'Editar item';
+      this.valorItem = this.ItemQueSeraEditado.nome;
+    }
   }
 }
--- life-cycle/src/app/components/item/item.component.ts
+++ life-cycle/src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -10,17 +10,24 @@ import { Item } from 'src/app/interfaces/iItem';
 export class ItemComponent implements OnInit, OnChanges {
 
   @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log('OnInit')
+
    }
 
    ngOnChanges(): void {
-    console.log('OnChanges')
+
+  }
+
+  editarItem()
+  {
+    this.emitindoItemParaEditar.emit(this.item);
   }
 
 }
